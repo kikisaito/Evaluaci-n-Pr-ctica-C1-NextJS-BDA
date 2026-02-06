c2e54eda8cbac132eaa29f399b58d2b736c98e22
Kikis <jaitovichbidnaud@gmail.com>
2026-02-05 21:08:17 -0600

    creacion de rol app_user con acceso restringido a unicamente VIEWS

---
+++ db/roles.sql
@@ -0,0 +1,15 @@
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'app_user') THEN
+        CREATE ROLE app_user WITH LOGIN PASSWORD 'cafeteria_pass_2024';
+    END IF;
+END $$;
+
+
+GRANT USAGE ON SCHEMA public TO app_user;
+GRANT SELECT ON ALL VIEWS IN SCHEMA public TO app_user;
+
+
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM app_user;
+GRANT SELECT ON ALL VIEWS IN SCHEMA public TO app_user;
